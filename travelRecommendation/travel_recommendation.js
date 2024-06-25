ac5e4f287948bc5a260e7fc8f935b550fc42686f
OluwatopeF <topeumar@gmail.com>
2024-06-25 14:15:06 -0400

    "message"

--- travelRecommendation/travel_recommendation.js
+++
@@ -1,7 +0,0 @@
-let btnReset = document.querySelector('button');
-let input = document.querySelector('input');
-
-btnReset.addEventListener('click', () =>{
-    input.forEach(input => input.value = '');
-});
-
